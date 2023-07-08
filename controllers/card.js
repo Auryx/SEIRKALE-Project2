155e26321e18f660d33f8f09227e19adc87d4b19
Auryx <lilbizb1@gmail.com>
2023-07-08 11:34:37 -0700

    completed base show route

--- controllers/card.js
+++ controllers/card.js
@@ -10,4 +10,12 @@ router.get('/', async (req, res) => {
     res.render('./card/index.ejs', {cards: allCards, })
 })
 
+
+// SHOW // THIS GOES AT THE BOTTOM OF THE GETS
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const card = await Card.findById(id)
+    res.render('./card/show.ejs', {card})
+})
+
 module.exports = router
--- models/seed.js
+++ models/seed.js
@@ -10,7 +10,12 @@ mongoose.connection.on('open', async () => {
 
     //1b. then using startFruits, we will insert that into the db
     const startCard = [{ 
-        name: "Myrel, Shield of Argive", type_line: "Legendary Creature - Human Soldier", expansion_symbol: "The Brothers' War (Mythic Rare)", artist_info: "Ryan Pancoast", power_toughness: "3/4", collectors_number: "18",
+        name: "Myrel, Shield of Argive", 
+        type_line: "Legendary Creature - Human Soldier", 
+        expansion_symbol: "The Brothers' War (Mythic Rare)", 
+        artist_info: "Ryan Pancoast", 
+        power_toughness: "3/4", 
+        collectors_number: "18",
         text_box: {
             abilities: `During your turn, your opponents can't cast spells or activate abilities of artifacts, creatures, or enchantments. Whenever Myrel, Shield of Argive attacks, create X 1/1 colorless Soldier artifact creature tokens, where X is the number of Soldiers you control.`,
             flavor_text: " "
@@ -20,7 +25,12 @@ mongoose.connection.on('open', async () => {
             {color_identity: "White"}
         ]},
         {
-        name: "Force of Will", type_line: "Instant", expansion_symbol: "Dominaria Remastered (Mythic Rare)", artist_info: "Donato Giancola", power_toughness: " ", collectors_number: "50",
+        name: "Force of Will", 
+        type_line: "Instant", 
+        expansion_symbol: "Dominaria Remastered (Mythic Rare)", 
+        artist_info: "Donato Giancola", 
+        power_toughness: "", 
+        collectors_number: "50",
         text_box: {
             abilities: `You may pay 1 life and exile a blue card from your hand rather than pay this spell's mana cost. Counter target spell.`,
             flavor_text: "Your artillery will burn itself out before I allow my focus to waver."
